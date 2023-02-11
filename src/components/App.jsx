4a3bef97a745dc069c3d2cf800d21e6392812659
Ivan Alimenko <ivan.alimenko@ukr.net>
2023-02-11 23:21:14 +0200

    commit_finaly

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,76 +1,69 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions.jsx';
 import { Statistics } from './Statistics/Statistics.jsx';
 import { Section } from './Section/Section.jsx';
 import { Notification } from './Notification/Notification.jsx';
 
-export class App extends Component {
-  state = {
+export const App = () => {
+  const [feedbacks, setFeedbacks] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
-  };
+  });
 
-  countTotalFeedback() {
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
-    return total;
-  }
+  const total = feedbacks.good + feedbacks.neutral + feedbacks.bad;
 
-  countPositiveFeedbackPercentage() {
-    const total = this.countTotalFeedback();
-    if (!total) {     // cheking for exclude NaN% in result if state = 0
-      return 0;
-    }
-    const value = this.state.good;
-    const result = Math.round((value / total) * 100);
-    return Number(result);
-  }
-
-  onLeaveFeedback = stateName => {
-    this.setState(prevState => {
-      return { [stateName]: prevState[stateName] + 1 };
+  const onLeaveFeedback = name => {
+    setFeedbacks(prevState => {
+      const value = prevState[name];
+      return { ...prevState, [name]: value + 1 };
     });
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage();
+  const countPositiveFeedbackPercentage = () => {
+    if (!total) {
+      // cheking for exclude NaN% in result if state = 0
+      return 0;
+    }
+    const value = feedbacks.good;
+    const result = Math.round((value / total) * 100);
+    return Number(result);
+  };
 
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.onLeaveFeedback}
-          />
-        </Section>
+  const positivePercentage = countPositiveFeedbackPercentage();
 
-        <Section title="Statistics">
-          {total !== 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={positivePercentage}
-            ></Statistics>
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys(feedbacks)}
+          onLeaveFeedback={onLeaveFeedback}
+        />
+      </Section>
+
+      <Section title="Statistics">
+        {total !== 0 ? (
+          <Statistics
+            good={feedbacks.good}
+            neutral={feedbacks.neutral}
+            bad={feedbacks.bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          ></Statistics>
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </div>
+  );
+};
